5b4b9af5f801a042f2adf5c8bab4725e00d3cb35
sephyshibu <sephyshibu@gmail.com>
2025-07-15 10:13:55 +0530

    ninja id card created with stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,111 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home:Ninja()
+
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Ninja extends StatefulWidget {
+ 
+  @override
+  State<Ninja> createState() => _NinjaState();
+}
 
-  // This widget is the root of your application.
+class _NinjaState extends State<Ninja> {
+  int count=0;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Ninja ID card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      floatingActionButton: FloatingActionButton( //when i click floating button then only count increases
+        onPressed: () {
+          setState(() {
+            count+=1;
+          });
+
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 20,30, 0),
+        
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assest/image.jpg'),
+                radius: 50,
+              ),
             ),
+            Divider(height: 20, thickness: 5, color: Colors.grey[600]),
+            Text(
+              "Name",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+                fontSize: 20
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              "Sephy Jithin",
+              style: TextStyle(
+                color: Colors.amber,
+                letterSpacing: 2,
+                fontSize: 30
+              ),
+            ),
+            SizedBox(height: 30),
+             Text(
+              "Current Ninja Level ",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+                fontSize: 20
+              ),
+            ),
+            SizedBox(height: 6),
+            Text(
+              "$count",
+              style: TextStyle(
+                color: const Color.fromARGB(255, 224, 205, 146),
+                letterSpacing: 2,
+                fontSize: 30
+              ),
+            ),
+            SizedBox(height: 30,),
+            Row(
+
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'sephyjithin@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 20,
+                  ),
+                )
+              ],
+            )
+
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
